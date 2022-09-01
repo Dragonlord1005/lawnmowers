73f3fc676f05ef4fc7d7c22307a852c449a33d6f
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-09-01 19:35:08 +0000

    hi

--- components/Navbar/NavBar.tsx
+++ components/Navbar/NavBar.tsx
@@ -22,20 +22,20 @@ export default function NavBar() {
   ]
   return (
     <Navbar>
-      <Navbar.Toggle showIn="xs" />
+      <Navbar.Toggle showIn="xs" autoFocus />
       <Navbar.Content hideIn="xs">
         <NextLink href="/"><Navbar.Link>Home</Navbar.Link></NextLink>
         <NextLink href="/pricing"><Navbar.Link>Pricing</Navbar.Link></NextLink>
         <NextLink href="/contact"><Navbar.Link>Contact</Navbar.Link></NextLink>
         <NextLink href="/terms"><Navbar.Link>Terms</Navbar.Link></NextLink>
       </Navbar.Content>
-      <Navbar.Collapse showIn="xs">
-      {links.map((item, index) => (
-        <Navbar.CollapseItem key={item.name}>
-          <NextLink href={links.href as keyof typeof String}
-            <Link>{links.name}</Link>
-        </Navbar.CollapseItem>
-      ))};
+      <Navbar.Collapse>
+        {links.map((link) => (
+          <NextLink href={link.href} key={link.name}>
+            <Link>{link.name}</Link>
+          </NextLink>
+        ))}
+
       </Navbar.Collapse>
       <Navbar.Brand>
         Lawnmowers
