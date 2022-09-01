7506775acc9538903408ea32d71e8c60483fb98e
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-09-01 19:02:40 +0000

    Doesnt work  yet, wip

--- components/Navbar/NavBar.tsx
+++ components/Navbar/NavBar.tsx
@@ -1,18 +1,46 @@
-import { Navbar } from "@nextui-org/react";
+import { Navbar, Link } from "@nextui-org/react";
 import NextLink from "next/link"
 
 export default function NavBar() {
+  const links = [
+    {
+      name: "Home",
+      href: "/",
+    },
+    {
+      name: "Pricing",
+      href: "/pricing",
+    },
+    {
+      name: "Contact",
+      href: "/contact"
+    },
+    {
+      name: "Terms",
+      href: "/terms"
+    }
+  ]
   return (
     <Navbar>
-      <Navbar.Content>
+      <Navbar.Toggle showIn="xs" />
+      <Navbar.Content hideIn="xs">
         <NextLink href="/"><Navbar.Link>Home</Navbar.Link></NextLink>
         <NextLink href="/pricing"><Navbar.Link>Pricing</Navbar.Link></NextLink>
         <NextLink href="/contact"><Navbar.Link>Contact</Navbar.Link></NextLink>
-        <NextLink href="terms"><Navbar.Link>Terms</Navbar.Link></NextLink>
+        <NextLink href="/terms"><Navbar.Link>Terms</Navbar.Link></NextLink>
       </Navbar.Content>
-      <Navbar.Content>
-        <Navbar.Item>Lawnmowers</Navbar.Item>
-      </Navbar.Content>
+      <Navbar.Collapse showIn="xs">
+      {links.map((item, index) => (
+        <Navbar.CollapseItem key={item}>
+          <NextLink href={links}
+            <Link>{links.name}</Link>
+        </Navbar.CollapseItem>
+      ))};
+      </Navbar.Collapse>
+      <Navbar.Brand>
+        Lawnmowers
+      </Navbar.Brand>
+
     </Navbar>
   )
 }
