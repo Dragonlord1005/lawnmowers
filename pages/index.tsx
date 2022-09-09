236edfec64b7d1adb4894d6f773affe93e7154b4
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-09-09 20:45:18 +0000

    Trying container

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,19 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import NextLink from "next/link";
-import { Text, Link, Spacer } from "@nextui-org/react";
+import { Text, Link, Spacer, Container } from "@nextui-org/react";
 
 const Home: NextPage = () => {
   return (
     <>
-      <div className={styles.container}>
+      <div>
         <Head>
           <title>Lawnmowers</title>
           <meta name="description" content="We mow lawns" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <main className={styles.main}>
+        <Container alignItems="center" display="flex" justify="center" direction="column">
           <Text h1>Lawnmowers of Valentine</Text>
           <Text h2>We mow Lawns.</Text>
           <NextLink href="/pricing">
@@ -26,7 +26,7 @@ const Home: NextPage = () => {
           <NextLink href="/contact">
             <Link>Contact Us</Link>
           </NextLink>
-        </main>
+        </Container>
       </div>
     </>
   );
