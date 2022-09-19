5dab4fbf2ccd9865ad8c603dc47efb4c8a6d0060
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-09-18 18:19:47 -0600

    Removed comments

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://lawnmowers-git-sitemap-dragonlord1005.vercel.app',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
+  generateRobotsTxt: true,
 }
