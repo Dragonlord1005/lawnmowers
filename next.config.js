477f274075151fe775d495f69eaf82d4eeb6da7e
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-09-23 19:30:03 +0000

    yoo

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 // jshint esversion: 6
 /** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -9,4 +14,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
