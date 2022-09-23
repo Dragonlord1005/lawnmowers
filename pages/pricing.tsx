2c879b855a17fac109862e71a134bcb3ee787d06
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-09-23 18:47:34 +0000

    Formatted and moved dynamic around

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -4,34 +4,34 @@ import { Text, Loading } from "@nextui-org/react";
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
 
-const Price = dynamic(() => import("../components/price/price"), {
-  suspense: true,
-  ssr: true,
-});
-
 const Pricing: NextPage = () => {
-  return (
-      <div>
-        <Head>
-          <title>Pricing</title>
-          <meta name="description" content="Our Pricing" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+  const Price = dynamic(() => import("../components/price/price"), {
+    suspense: true,
+    ssr: true,
+  });
 
-        {/* <Container> */}
-          <Text
-            h1
-            css={{
-              textAlign: "center",
-            }}
-          >
-            Pricing
-          </Text>
-          <Suspense fallback={<Loading />}>
-            <Price />
-          </Suspense>
-        {/* </Container> */}
-      </div>
+  return (
+    <div>
+      <Head>
+        <title>Pricing</title>
+        <meta name="description" content="Our Pricing" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* <Container> */}
+      <Text
+        h1
+        css={{
+          textAlign: "center",
+        }}
+      >
+        Pricing
+      </Text>
+      <Suspense fallback={<Loading />}>
+        <Price />
+      </Suspense>
+      {/* </Container> */}
+    </div>
   );
 };
 
